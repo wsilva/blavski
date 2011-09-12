e24411527dbd44fea6cab4a6c44ab90c9ae144c2
Wellington Silva <wsilva@usp.br>
2011-09-11 23:20:58 -0300

    suporte para jquery

--- application/views/tmpl/header.php
+++ application/views/tmpl/header.php
@@ -5,7 +5,7 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> 
 
 <link href="/assets/css/main.css" media="screen" rel="stylesheet" type="text/css" /> 
- 
+<script type="text/javascript" src="/assets/js/jquery-1.6.3.min.js"></script>
 </head> 
 <body>
     
